b27ac61800cee94edde1c7e68a8d229e88be45a7
ert3e <samoilenkod@mail.ua>
2019-06-06 23:56:42 +0300

    Reduxe include architercture

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,22 @@ import './Resources/css/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
+import { Provider } from 'react-redux';
+import { creareStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+import Reducer from './reducers'
+
+const creareStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(creareStore);
+
+
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+    <Provider store={creareStoreWithMiddleware(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+        }>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
 ,
 document.getElementById('root'));
