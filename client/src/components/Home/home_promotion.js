1aee190af44344297c89b44391072795c215598a
ert3e <samoilenkod@mail.ua>
2019-06-24 03:06:12 +0300

    Add home promiton

--- client/src/components/Home/home_promotion.js
+++ client/src/components/Home/home_promotion.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import MyButtom from '../untils/button';
+import MyButton from '../untils/button';
 
 const HomePromotion = () => {
     const promotion = {
         img:'/images/featured/featured_home_3.jpg',
-        lineOne: 'Up to 40% off',
-        lineTwo: 'In second'
+        lineOne: 'Fendwer',
+        lineTwo: 'In second hand guitars',
+        linkTitle: 'Shop now',
+        linkTo: '/shop'
     }
+    const renderPromotion = () => (
+        promotion ?
+        <div className="home_promotion_img"
+            style={{
+                background:`url(${promotion.img})`
+            }}
+        >
+            <div className="tag title">{promotion.lineOne}</div>
+            <div className="tag low_title">{promotion.lineTwo}</div>
+            <div>
+                <MyButton
+                    type="default"
+                    title={promotion.linkTitle}
+                    linkTo={promotion.linkTo}
+                    addStyles={{
+                            margin:'10px 0 0 0'
+                    }}
+                     />
+            </div>
+        </div>
+        :null
+    )
     return (
-        <div>
-            
+        <div className="home_promotion">
+            {renderPromotion()}
         </div>
     );
 };
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import HomeSlider from './home_slider';
+import HomePromotion from './home_promotion';
+import { connect } from 'react-redux';
+
+import { getProductBySell, getProductsByArrival } from '../../actions/products_actions';
 
 class Home extends Component {
+    componentDidMount(){
+        this.props.dispatch(getProductBySell());
+    }
     render() {
         return (
             <div>
                 <HomeSlider/>
+                <HomePromotion/>
             </div>
         );
     }
 }
-
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+export default connect(mapStateToProps)(Home);
